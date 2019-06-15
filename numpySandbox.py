d7482c8e0b845dfaca2400eee74d6e8db3f18e66
Dshafique <dawudshafique95@gmail.com>
2019-06-15 00:16:38 -0500

    numpy stuff

---
+++ numpySandbox.py
@@ -0,0 +1,11 @@
+import numpy as np
+ # this program uses numpy to create an array of 10 zeros, 10 ones, & 10 fives.
+array=np.zeros(10)
+print("An array of 10 zeros:")
+print(array)
+array=np.ones(10)
+print("An array of 10 ones:")
+print(array)
+array=np.ones(10)*5
+print("An array of 10 fives:")
+print(array)
